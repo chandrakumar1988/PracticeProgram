82d6ceb202016fb8690766fe6059ac6d687dff06
Chandrakuamr <achandrakumar1988@gmail.com>
2023-07-12 17:26:08 +0530

    Stringreverse1

--- src/main/java/Stringrelatedprograms/StringReversedifferntway.java
+++ src/main/java/Stringrelatedprograms/StringReversedifferntway.java
@@ -2,4 +2,25 @@ package Stringrelatedprograms;
 
 public class StringReversedifferntway {
 
+    public static void main (String args[])
+    {
+        String reversestrg =Reversestringbycharatindex();
+        System.out.println(reversestrg);
+    }
+    public static String Reversestringbycharatindex()
+    {
+       String originalString ="Hello how are you";
+       int length=originalString.length();
+       char ch;
+       String reversestring ="";
+       for (int i=length-1;i>=0;i--)
+       {
+         ch  =originalString.charAt(i);
+           reversestring =reversestring+ch;
+       }
+      return reversestring;
+
+
+    }
+
 }
