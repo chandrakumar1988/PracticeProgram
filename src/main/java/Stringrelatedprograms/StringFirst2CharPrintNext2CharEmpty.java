7723af05f0e0650e04cdc89d871dc00c3541fdd1
Chandra Kumar <achandrakumar1988@gmail.com>
2023-07-13 15:19:49 +0530

    eeryfirst2charprintnext2charshouldbeempty.java

--- src/main/java/Stringrelatedprograms/StringFirst2CharPrintNext2CharEmpty.java
+++ src/main/java/Stringrelatedprograms/StringFirst2CharPrintNext2CharEmpty.java
@@ -2,7 +2,7 @@ package Stringrelatedprograms;
 
 public class StringFirst2CharPrintNext2CharEmpty {
     public static void main(String[] args) {
-        String str = "welcome";
+        String str = "welcome to the program world";
         String result = "";
 
         for (int i = 0; i < str.length(); i += 4) {
