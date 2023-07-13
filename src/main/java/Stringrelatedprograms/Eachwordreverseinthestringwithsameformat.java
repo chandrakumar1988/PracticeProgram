e351c10d95c267f079378c2ca71373629e99317c
Chandra Kumar <achandrakumar1988@gmail.com>
2023-07-13 15:19:21 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/Stringrelatedprograms/StringEveryfirst2charprintnext2charshouldbeempty.java

---
+++ src/main/java/Stringrelatedprograms/Eachwordreverseinthestringwithsameformat.java
@@ -0,0 +1,34 @@
+package Stringrelatedprograms;
+
+import javax.swing.*;
+
+public class Eachwordreverseinthestringwithsameformat {
+    public static void main (String args[])
+    {
+
+        String str ="Hi how are you chandra mathu";
+        String[] strarray =str.split(" ");
+        String reversestring ="";
+
+        for (String words:strarray)
+        {
+            String reverseword="";
+            for(int i=words.length()-1;i>=0;i--)
+            {
+
+                reverseword +=words.charAt(i);
+
+
+
+            }
+            reversestring+=reverseword+" ";
+
+
+
+        }
+        System.out.println(reversestring);
+
+    }
+
+
+}
