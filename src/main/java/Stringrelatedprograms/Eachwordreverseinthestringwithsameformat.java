ba68f35f54c075bf26b1b2a0c12cbc8f3cde48b5
Chandra Kumar <achandrakumar1988@gmail.com>
2023-07-26 06:17:05 +0530

    Practice program

--- src/main/java/Stringrelatedprograms/Eachwordreverseinthestringwithsameformat.java
+++ src/main/java/Stringrelatedprograms/Eachwordreverseinthestringwithsameformat.java
@@ -3,32 +3,24 @@ package Stringrelatedprograms;
 import javax.swing.*;
 
 public class Eachwordreverseinthestringwithsameformat {
-    public static void main (String args[])
-    {
+    public static void main(String args[]) {
 
-        String str ="Hi how are you chandra mathu";
-        String[] strarray =str.split(" ");
-        String reversestring ="";
+        String str = "Hi how are you chandra mathu";
+        String[] strarray = str.split(" ");
+        String reversestring = "";
 
-        for (String words:strarray)
-        {
-            String reverseword="";
-            for(int i=words.length()-1;i>=0;i--)
-            {
+        for (String words : strarray) {
+            String reverseword = "";
+            for (int i = words.length() - 1; i >= 0; i--) {
 
-                reverseword +=words.charAt(i);
-
-
+                reverseword += words.charAt(i);
 
             }
-            reversestring+=reverseword+" ";
-
-
+            reversestring += reverseword + " ";
 
         }
         System.out.println(reversestring);
 
     }
-
 
 }
--- src/main/java/Stringrelatedprograms/StringEveryfirst2charprintnext2charshouldbeempty.java
+++ src/main/java/Stringrelatedprograms/StringEveryfirst2charprintnext2charshouldbeempty.java
@@ -1,24 +1,23 @@
 package Stringrelatedprograms;
 
-public class StringEveryfirst2charprintnext2charshouldbeempty {
-public static void main(String[] args) {
-    String str = "welcome chandra kumar";
-    String result = "";
+public class StringEveryfirst2charprintnext2charshouldbeempty
+{
+    public static void main(String[] args) {
+        String str = "welcome chandra kumar";
+        String result = "";
 
-    for (int i = 0; i < str.length(); i += 4) {
-        if (i < str.length()) {
-            result += str.charAt(i);
+        for (int i = 0; i < str.length(); i += 4) {
+            if (i < str.length()) {
+                result += str.charAt(i);
+            }
+            if (i + 1 < str.length()) {
+                result += str.charAt(i + 1);
+            }
+            result += "  ";
         }
-        if (i + 1 < str.length()) {
-            result += str.charAt(i + 1);
-        }
-        result += "  ";
+
+        System.out.println(result);
     }
-
-    System.out.println(result);
-}
-
-
 
 }
 
