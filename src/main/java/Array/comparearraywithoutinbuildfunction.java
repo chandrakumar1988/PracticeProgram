2ef5c50bf5b4fb8b8226c03cf6bbfc72d274110c
Chandra Kumar <achandrakumar1988@gmail.com>
2023-07-26 06:35:59 +0530

    Practice program

--- src/main/java/Array/comparearraywithoutinbuildfunction.java
+++ src/main/java/Array/comparearraywithoutinbuildfunction.java
@@ -1,5 +1,31 @@
 package Array;
 
 public class comparearraywithoutinbuildfunction {
+    public static void main(String[] args) {
+        String[] a = {"chandra", "kumar", "anbu"};
+        String[] b = {"chandra", "kumar", "anbu"};
 
+        if (comparearrayeaquals(a, b)) {
+            System.out.println("Arrays are equal");
+        } else {
+            System.out.println("Arrays not are equal");
+        }
+    }
+
+    public static boolean comparearrayeaquals(String[] a, String[] b) {
+        if (a.length != b.length) {
+            return false;
+        } else {
+            for (int i = 0; i < a.length; i++) {
+                if (a[i] != b[i]) {
+                    return false;
+                }
+
+            }
+            return true;
+
+        }
+    }
 }
+
+
